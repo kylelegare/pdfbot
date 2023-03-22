a85e1714e7883ec60e6041fbdab06d6b9c2d17f5
Kyle Legare <kylelegare@gmail.com>
2023-03-21 18:29:36 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from utils import parse_pdf, embed_text, get_answer
+
+
+# Load custom CSS
+st.markdown("""
+    <style>
+        %s
+    </style>
+""" % open("style.css").read(), unsafe_allow_html=True)
+
+col1, col2 = st.columns([1, 4])
+with col1:
+    st.image("images/pdfbot.png")
+with col2:
+    st.header("PDF Bot")
+uploaded_file = st.file_uploader("Upload any PDF and ask questions about the document", type=["pdf"])
+
+if uploaded_file is not None:
+    index = embed_text(parse_pdf(uploaded_file))
+    query = st.text_area("Ask a question about the document")
+    button = st.button("Submit")
+    if button:
+        result = get_answer(index, query)
+        output_text = result['result']
+        output_paragraphs = output_text.split('\n\n')
+        formatted_text = '\n\n'.join([f'<p>{p}</p>' for p in output_paragraphs])
+        st.write(f"Your results:", unsafe_allow_html=True)
+        st.markdown(formatted_text, unsafe_allow_html=True)
+
---
+++ utils.py
@@ -0,0 +1,75 @@
+import os
+import streamlit as st
+from pypdf import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import VectorDBQA
+from langchain.chat_models import ChatOpenAI
+from langchain import PromptTemplate, LLMChain
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import (
+    AIMessage,
+    HumanMessage,
+    SystemMessage
+)
+chat = ChatOpenAI(temperature=0)
+
+#Set OpenAI API Key
+openai_api_key = st.secrets['OPENAI_API_KEY']
+
+
+@st.cache
+def parse_pdf(file):
+    pdf = PdfReader(file)
+    output = []
+    for page in pdf.pages:
+        text = page.extract_text()
+        output.append(text)
+
+    return "\n\n".join(output)
+
+
+@st.cache
+def embed_text(text):
+    """Split the text and embed it in a FAISS vector store"""
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800, chunk_overlap=0, separators=["\n\n", ".", "?", "!", " ", ""]
+    )
+    texts = text_splitter.split_text(text)
+
+    embeddings = OpenAIEmbeddings()
+    index = FAISS.from_texts(texts, embeddings)
+
+    return index
+
+
+def get_answer(index, query):
+    system_template = "You are a helpful assistant analyzing a document someone gave you"
+    messages = [
+        SystemMessagePromptTemplate.from_template(system_template),
+        HumanMessagePromptTemplate.from_template("{context}"),
+    ]
+    prompt = ChatPromptTemplate.from_messages(messages)
+
+    chain_type_kwargs = {"prompt": prompt}
+    chain = VectorDBQA.from_chain_type(
+        llm=ChatOpenAI(),
+        chain_type="stuff",
+        vectorstore=index,
+        chain_type_kwargs=chain_type_kwargs,
+    )
+
+    answer = chain({"query": query}, return_only_outputs=True)
+
+    return answer
+
+
+
+
+
